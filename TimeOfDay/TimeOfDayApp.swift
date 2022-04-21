3b6704fe9d4d4daa1d51991f2e04e5204c95b393
Ricky Austin <ricky.austin@koinworks.com>
2022-04-21 11:23:55 +0700

    Initial Commit

---
+++ TimeOfDay/TimeOfDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeOfDayApp.swift
+//  TimeOfDay
+//
+//  Created by Ricky Austin on 21/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TimeOfDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
